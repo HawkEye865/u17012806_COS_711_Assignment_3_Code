99e761bbe63cef8668ebfdb532ca04c5d1253db8
HawkEye865 <u17012806@tuks.co.za>
2021-11-11 13:59:55 +0200

    changes

--- Main.py
+++ Main.py
@@ -1,20 +1,13 @@
-from numpy.core.arrayprint import FloatingFormat
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-from keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.utils import img_to_array
-from tensorflow.keras.utils import array_to_img
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
-from keras_preprocessing import image
-from keras.preprocessing.image import load_img
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.preprocessing.image import load_img
 from PIL import Image
 import numpy
-import statistics as stats
-from datetime import datetime
 import time
+
 
 problem_file = "Train.csv"
 X_traintmp = []
@@ -24,6 +17,13 @@ y_testtmp = []
 imgsize = 100
 imgtraincount = 3406
 imgvalcount = 3406
+
+kernel = 1
+learn_rate = 0.001
+activation_function = 'relu'
+output_activation_function = 'softmax'
+error_function = 'categorical_crossentropy'
+
 try:
     with open(problem_file, 'r') as f:
         f.readline()
@@ -103,37 +103,38 @@ y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
 results = open("results.txt", "a")
-results.write("Train-----TrainError-----Test-----Time\n")
-
 #create model
 start = time.time()
 model = Sequential()
 #add model layers
-model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(imgsize,imgsize,3)))
-model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Conv2D(64, kernel_size=kernel, activation=activation_function, input_shape=(imgsize,imgsize,3)))
+model.add(Conv2D(32, kernel_size=kernel, activation=activation_function))
 model.add(MaxPooling2D())
-model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Conv2D(32, kernel_size=kernel, activation=activation_function))
 model.add(MaxPooling2D())
 model.add(Flatten())
-model.add(Dense(3, activation='softmax'))
+model.add(Dense(3, activation=output_activation_function))
 
 #compile model using accuracy to measure model performance
-model.compile(optimizer=tf.optimizers.Adam(learning_rate=0.001,beta_1=0.9,beta_2=0.999,epsilon=1e-07,amsgrad=False,name='Adam'), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=tf.optimizers.Adam(learning_rate=learn_rate,beta_1=0.9,beta_2=0.999,epsilon=1e-07,amsgrad=False,name='Adam'), loss=error_function, metrics=['accuracy'])
 
-# #train the model
+#train the model
 history = model.fit(X_train, y_train, epochs=60)
 end = time.time()
 elapsedTime = end - start
 elapsedTime = elapsedTime/60
 
+#test the model
 out = model.predict(X_test[:1000])
 outstr = []
 tarstr = []
 
 for x in out:
-    outstr.append(str(round(x[0])) + str(round(x[1])) + str(round(x[2])))
+    max_index = numpy.where(x == numpy.amax(x))
+    outstr.append(str(max_index))
 for x in y_test:
-    tarstr.append(str(round(x[0])) + str(round(x[1])) + str(round(x[2])))
+    max_index = numpy.where(x == numpy.amax(x))
+    tarstr.append(str(max_index))
 
 correct = 0
 for i in range(len(outstr)):
@@ -141,5 +142,6 @@ for i in range(len(outstr)):
         correct += 1
 
 accuracy2 = correct/len(out)*100
+print(accuracy2)
 results.write(str(history.history['accuracy'][-1]*100) + "-----" + str(history.history['loss'][-1]) + "-----" + str(accuracy2) + "-----" + str(elapsedTime)+"\n")
 results.close() 
